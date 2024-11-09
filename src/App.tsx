25db09f68d77c9b024659ba245aaa1b07a18cbf1
Mateusz Migas <54471371+mateuszmigas@users.noreply.github.com>
2024-11-09 15:04:50 +0100

    add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import { Canvas } from "@react-three/fiber";
 import { GameCanvas } from "./canvas";
 import { useState } from "react";
+import { Button } from "./components/ui/button";
 
 export const App = () => {
   const [isOrthographic, setIsOrthographic] = useState(false);
 
   return (
     <div className="size-full relative">
-      <button
-        className="absolute top-4 right-4 bg-white/10 hover:bg-white/20 text-white px-4 py-2 rounded-lg z-10"
-        onClick={() => setIsOrthographic(!isOrthographic)}
-      >
-        {isOrthographic ? "Perspective View" : "Top View"}
-      </button>
       <Canvas className="size-full bg-gray-900">
         <GameCanvas isOrthographic={isOrthographic} />
       </Canvas>
+      <Button
+        className="absolute top-4 right-4"
+        onClick={() => setIsOrthographic(!isOrthographic)}
+      >
+        {isOrthographic ? "Perspective View" : "Top View"}
+      </Button>
     </div>
   );
 };
